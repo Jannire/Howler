611a8f64d4bf1572bf4f72ba8d7a525a3bc557b3
Jannire Trujillo <88001855+Jannire@users.noreply.github.com>
2022-04-08 20:14:42 -0500

    Update howler.py

--- howler.py
+++ howler.py
@@ -22,6 +22,22 @@ async def on_message(message):
   if message.content.startswith('zzz'):
     await message.channel.send('https://www.youtube.com/watch?v=E2t9jUyra7s')
 
+#gifs
+  if message.content.startswith('inception'):
+    await message.channel.send('https://tenor.com/view/leonardo-di-caprio-inception-gif-9086835')
+  if message.content.startswith('tenet'):
+    await message.channel.send('https://tenor.com/view/tenet-gif-18707553')
+  if message.content.startswith('dobby'):
+    await message.channel.send('https://tenor.com/view/dance-dobbi-harry-potter-meme-gif-16801919')
+  if message.content.startswith('harry'):
+    await message.channel.send('https://tenor.com/view/harry-potter-harrypotter-ron-hermione-gif-16137638')
+  if message.content.startswith('piero'):
+    await message.channel.send('https://tenor.com/view/pingu-mad-stare-angry-not-happy-death-glare-gif-23558934')
+  if message.content.startswith('midsommar'):
+    await message.channel.send('https://tenor.com/view/midsommar-dancing-turning-spinning-gif-15685623')
+  if message.content.startswith('mitty'):
+    await message.channel.send('https://tenor.com/view/the-secret-life-of-walter-mitty-comedy-drama-adventure-ben-stiller-gif-3454082')
+
 #tok -> token del bot
 secret = os.environ['tok']
 client.run(secret)
